b739d842f3c83236b90a1cb74b95e2c7f4170088
Mishal <mishal0404@gmail.com>
2024-04-28 23:35:14 +0530

    backend done

--- web/backend/server.js
+++ web/backend/server.js
@@ -10,9 +10,7 @@ const app = express();
 
 app.use(cors({origin: "*"}));
 
-
-app.get('/getgame', async (req, res) => {
-
+async function connect(){
   const uri = "mongodb+srv://mishal0404:mishal2003@mishal0404.35lsnon.mongodb.net/?retryWrites=true&w=majority";
   
   const client = new MongoClient(uri, {
@@ -25,11 +23,108 @@ app.get('/getgame', async (req, res) => {
 
 
   await client.connect();
-  const database = client.db("shifa");
-  const collection = database.collection("usertimes");
+  const database = client.db("elite-arena");
+  const collection = database.collection("events");
+  return collection
+}
 
-  res.status(200).send({"newUser":"sdfs"});
+
+app.get('/get_all_events', async (req, res) => {
+
+  const collection = connect()
+  const cursor = collection.find({})
+  const result = await cursor.toArray();
+  res.status(200).send(result);
 });
+
+app.post('/get_my_participations', async (req, res) => {
+  username = req.body.username
+  
+  const collection = connect()
+  const cursor = collection.find({ 'participants.name': username })
+  const result = await cursor.toArray();
+  res.status(200).send(result);
+});
+
+app.post('/create_team', async (req, res) => {
+  const { eventName, teamName, username } = req.body
+
+  const collection = connect()
+  
+  const event = await collection.findOne({ name: eventName });
+
+  const currentDate = new Date();
+  const registrationDeadline = new Date(event.reglastdate);
+  if (currentDate > registrationDeadline) {
+    return res.status(400).json({ error: 'Registration deadline has passed' });
+  }
+
+  const existingTeam = event.participants.find(participant => participant.team === teamName);
+  if (existingTeam) {
+    return res.status(400).json({ error: 'Team already exists for this event' });
+  }
+
+  const newParticipant = { name: username, team: teamName };
+  await collection.updateOne({ name: eventName }, { $push: { participants: newParticipant } });
+  
+
+  res.status(200).json({ message: 'User added to the team successfully' });
+});
+
+
+app.post('/join_team', async (req, res) => {
+  const { eventName, teamName, username } = req.body
+
+  const collection = connect()
+  
+  const event = await collection.findOne({ name: eventName });
+
+  const currentDate = new Date();
+  const registrationDeadline = new Date(event.reglastdate);
+  if (currentDate > registrationDeadline) {
+    return res.status(400).json({ error: 'Registration deadline has passed' });
+  }
+
+  const existingTeam = event.participants.find(participant => participant.team === teamName);
+  if (!existingTeam) {
+    return res.status(404).json({ error: 'Team not found for this event' });
+  }
+
+  const newParticipant = { name: username, team: teamName };
+  await collection.updateOne({ name: eventName }, { $push: { participants: newParticipant } });
+  
+
+  res.status(200).json({ message: 'User joined the team successfully' });
+});
+
+app.post('/create_game', async (req, res) => {
+  const { name, team, ppt, organizer, reglastdate, startdate, maxteams, prizes } = req.body;
+
+  const collection = connect()
+  
+  const existingEvent = await collection.findOne({ name });
+  if (existingEvent) {
+    return res.status(400).json({ error: 'Event with this name already exists' });
+  }
+
+  const newGame = {
+    name,
+    team,
+    ppt,
+    organizer,
+    participants: [],
+    reglastdate,
+    startdate,
+    maxteams,
+    prizes
+  };
+
+  await collection.insertOne(newGame);  
+
+  res.status(200).json({ message: 'Event created successfully' });
+});
+
+
 
 
 app.listen(port, () => {
