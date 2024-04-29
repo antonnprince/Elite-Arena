ecf262a41782067a237ec80355b8f01c1dbc7b6d
antonnprince <antonprince95@gmail.com>
2024-04-29 23:54:06 +0530

    modal done

--- web/frontend/src/Browse.jsx
+++ web/frontend/src/Browse.jsx
@@ -9,9 +9,9 @@ import tourn from "./images/popular-07.jpg"
 
 const Browse = () => {
   const [user, setUser] = useState(null);
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
   const [showModal, setShowModal] = useState(false);
-
+  const [focus, setFocus] = useState(null)
   
   const getdata = async () =>{
     const response = await fetch("http://localhost:3000/get_all_events", {
@@ -19,8 +19,8 @@ const Browse = () => {
     });
 
     const datak = await response.json()
-    setData(datak)
-    console.log(datak)
+    setData(datak.data)
+    console.log(datak.data)
   }
   useEffect(() => {
     const auth = getAuth();
@@ -48,23 +48,24 @@ const Browse = () => {
     <div className='flex flex-row flex-wrap space-x-4 space-y-8 h-full mx-24'>
       <br/>
     {
-      data.map((each)=>{
+      data.map((each,index)=>{
         return(
-          <div className='flex-col'>
+          <div className='flex-col' key={data._id}>
           <img src={each.image} key={each._id} className='w-42 h-56 rounded-xl my-4'/>
-          <h6 className='text-white font-normal text-xl'>{each.name}</h6>
+          <h4 className='text-white font-normal text-xl'>{each.name}</h4>
           <>
         <button
         className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
         type="button"
-        onClick={() => {setShowModal(true)
+        onClick={() => {setShowModal(true);
+        setFocus(each);
         }}
       >
         More details
       </button>
 
       {
-        showModal ? (
+        showModal && focus &&(
         <>
           <div className="justify-center items-center flex bg-zinc-800 overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
@@ -72,10 +73,13 @@ const Browse = () => {
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg bg-zinc-800 relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
-                    {data[0].game}
+                <div className="flex items-start justify-between p-5 ">
+                  <div className='flex flex-col items-start mx-auto'>
+                  <img src={focus.image} className='w-3/4 h-42 bg-zinc-800 p-4 rounded-3xl'/>
+                  <h3 className="text-3xl font-bold text-pink-500">
+                    {focus.name}
                   </h3>
+                  </div>
                   <button
                     className="p-1 ml-auto bg-black border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}
@@ -86,14 +90,23 @@ const Browse = () => {
                   </button>
                 </div>
                 {/*body*/}
-                <div className="relative p-6 flex-auto bg-zinc-800 text-white mx-2 rounded-3xl">
-                  <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                <div className="relative p-6 flex-auto bg-zinc-800 text-white mx-4 rounded-3xl">
+                  <p className="my-4 text-blueGray-500 text-lg leading-relaxed flex flex-col">
                     I always felt like I could do anything. That’s the main
                     thing people are controlled by! Thoughts- their perception
                     of themselves! They're slowed down by their perception of
                     themselves. If you're taught you can’t do anything, you
                     won’t do anything. I was taught I could do everything.
                   </p>
+                  <h4 className='text-white font-semibold'><span className='text-pink-500'>Max teams allowed:</span>{focus.maxteams} </h4>
+                  <h4 className='text-white font-semibold'><span className='text-pink-500'>Game used:</span>{focus.game} </h4>
+                  <h4 className='text-white font-semibold'><span className='text-pink-500'>Total participants:</span>{focus.participants.length} </h4>
+                  <h4 className='text-white font-semibold'><span className='text-pink-500'>Participants per Team:</span>{focus.ppt} </h4>
+                  <h4 className='text-white font-semibold'><span className='text-pink-500'>First Prize:</span>{focus.prizes["first"]} </h4>
+                  <h4 className='text-white font-semibold'><span className='text-pink-500'>Second Prize:</span>{focus.prizes["second"]} </h4>
+                  <h4 className='text-white font-semibold'><span className='text-pink-500'>Third Prize:</span>{focus.prizes["third"]} </h4>
+                  <h4 className='text-white font-semibold'><span className='text-pink-500'>Last Date to Register:</span>{focus.reglastdate} </h4>
+                  <h4 className='text-white font-semibold'><span className='text-pink-500'>Tournament Begins At:</span>{focus.startdate} </h4>
                 </div>
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
@@ -109,7 +122,7 @@ const Browse = () => {
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
-                    Save Changes
+                    Join Tournament
                   </button>
                 </div>
               </div>
@@ -117,7 +130,7 @@ const Browse = () => {
           </div>
           <div className="opacity-25 fixed inset-0 z-40 bg-zinc-900"></div>
         </>
-         ) : null}
+         )}
         </>
           </div>
         )
