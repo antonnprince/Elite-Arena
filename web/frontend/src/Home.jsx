ce90e3c0fb2f475bcaea1c8c17f922444c084178
antonnprince <antonprince95@gmail.com>
2024-04-29 11:50:19 +0530

    session

--- web/frontend/src/Home.jsx
+++ web/frontend/src/Home.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import 'firebase/auth';
-import { app, auth } from './config';
+// import { getAuth, onAuthStateChanged } from 'firebase/auth';
+// import 'firebase/auth';
+// import { app, auth } from './config';
 import { useEffect, useState } from "react";
 const Home = () => {
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
   
-  useEffect(() => {
-    const auth = getAuth();
+  // useEffect(() => {
+  //   const auth = getAuth();
 
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      if (currentUser) {
-        console.log(currentUser)
-        setUser(currentUser);
-      } else {
-        window.location.href = "/login"
-      }
-    });
+  //   const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+  //     if (currentUser) {
+  //       console.log(currentUser)
+  //       setUser(currentUser);
+  //     } else {
+  //       window.location.href = "/login"
+  //     }
+  //   });
 
-    return () => unsubscribe();
-  }, []);
+  //   return () => unsubscribe();
+  // }, []);
   return (
    <div className='bg-zinc-800 rounded-3xl px-8 py-4 h-auto
    flex flex-col space-y-8
--- web/frontend/src/Nav.jsx
+++ web/frontend/src/Nav.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import logo from './images/mainlogo.png'
-import {Outlet, Link} from "react-router-dom"
+import React, { useState, useEffect } from 'react';
+import logo from './images/mainlogo.png';
+import { Outlet, Link } from "react-router-dom";
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import 'firebase/auth';
-import { app, auth } from './config';
-import { useEffect, useState } from "react";
+import { auth } from './config';
+
 const Nav = () => {
   const [user, setUser] = useState(null);
   
   useEffect(() => {
-    const auth = getAuth();
-
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+    const authInstance = getAuth();
+    const unsubscribe = onAuthStateChanged(authInstance, (currentUser) => {
       if (currentUser) {
-        console.log(currentUser)
         setUser(currentUser);
       }
     });
@@ -23,55 +20,43 @@ const Nav = () => {
 
   const handleSignOut = () => {
     auth.signOut().then(() => {
-      window.location.href = "/login"
+      window.location.href = "/login";
     });
   };
-  // handleColor=()=>{
 
-  // }
   return (
     <>
-       <nav className='flex flex-row justify-between py-4'>
-      <img src={logo} alt='Elite Arena logo' 
-        className='w-42 h-16 '
-      />
+      <nav className='flex flex-row justify-between py-4'>
+        <img src={logo} alt='Elite Arena logo' className='w-42 h-16' />
 
-      <div className='text-pink-500 text-lg '>
-        <ul className='flex flex-row space-x-16'>
+        <div className='text-pink-500 text-lg '>
+          <ul className='flex flex-row space-x-16'>
             <li className='text-gray-600 hover:text-pink-500 delay-200 hover:cursor-pointer'>
-            <Link to="/">Home</Link>
+              <Link to="/">Home</Link>
             </li>
-          <li className='text-gray-600 hover:text-pink-500 delay-200 hover:cursor-pointer'>
-            <Link to="/browse">
-            Browse
-            </Link>
-          </li>
-          {!user && <li className='text-gray-600 hover:text-pink-500 delay-200 hover:cursor-pointer'>
-            <Link to="/login">
-              Login
-            </Link>
-            
-          </li>}
-          {user && <li className='text-gray-600 hover:text-pink-500 delay-200 hover:cursor-pointer'>
-            <Link onClick={handleSignOut}>
-              Logout
-            </Link>
-            
-          </li>}
-          <li className='text-gray-600 hover:text-pink-500 delay-200 hover:cursor-pointer'>
-            <Link to="/profile">
-            Profile
-            </Link>
-          </li>
-         
-        </ul>
-      </div>
-   </nav>
+            <li className='text-gray-600 hover:text-pink-500 delay-200 hover:cursor-pointer'>
+              <Link to="/browse">Browse</Link>
+            </li>
+            {!user && (
+              <li className='text-gray-600 hover:text-pink-500 delay-200 hover:cursor-pointer'>
+                <Link to="/login">Login</Link>
+              </li>
+            )}
+            {user && (
+              <li className='text-gray-600 hover:text-pink-500 delay-200 hover:cursor-pointer'>
+                <Link onClick={handleSignOut}>Logout</Link>
+              </li>
+            )}
+            <li className='text-gray-600 hover:text-pink-500 delay-200 hover:cursor-pointer'>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
-   <Outlet/>
-</>
-
-  )
+      <Outlet />
+    </>
+  );
 }
 
-export default Nav
+export default Nav;
