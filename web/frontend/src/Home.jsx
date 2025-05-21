bf01dd97fb277798219f6ef0bf76013254d60b6a
antonnprince <antonprince95@gmail.com>
2025-05-21 16:26:26 +0530

    update

--- web/frontend/src/Home.jsx
+++ web/frontend/src/Home.jsx
@@ -1,31 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-// import { getAuth, onAuthStateChanged } from 'firebase/auth';
-// import 'firebase/auth';
-// import { app, auth } from './config';
-import { useEffect, useState } from "react";
 const Home = () => {
-  // const [user, setUser] = useState(null);
-  
-  // useEffect(() => {
-  //   const auth = getAuth();
-
-  //   const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-  //     if (currentUser) {
-  //       console.log(currentUser)
-  //       setUser(currentUser);
-  //     } else {
-  //       window.location.href = "/login"
-  //     }
-  //   });
-
-  //   return () => unsubscribe();
-  // }, []);
 
   return (
     <>
    <div className='bg-zinc-800 rounded-3xl px-8 py-4 h-auto
-   flex flex-col space-y-8 overflow-x-hidden
+   flex flex-col space-y-8 overscroll-x-none 
    '>
       <div className='bg-[url("src/images/banner-bg.jpg")]
       bg-no-repeat bg-cover rounded-3xl
@@ -55,13 +35,6 @@ const Home = () => {
           </Link>
         </button>
     </div>
-
-    {/* Most Popular Right Now */}
-    {/* <div className='bg-zinc-900 px-24 py-4 rounded-3xl'>
-        <h1 className='text-white font-bold text-4xl'>Featured <span className='text-pink-400'>Games</span></h1>
-        <p className='text-white text-lg'></p>
-        <button className='bg-pink-500 text-white text-center px-4 py-2 font-bold rounded-full mx-[600px]'>Start Tournament</button>
-    </div> */}
    </div>
    </>
   )
